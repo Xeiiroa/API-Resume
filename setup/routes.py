b4213c5c6366d7df682eb9b665ad0f8c178c8345
Sintiria <Jaydentomlinson4@gmail.com>
2023-11-24 12:18:05 -0500

    updated routes.py
    added info for profile and projects

--- setup/routes.py
+++ setup/routes.py
@@ -24,11 +24,17 @@ def profile(): #returns social media links
     github
     linkedin
     """
-    
+    profile = {name: os.getenv('FULL_NAME'),
+    githubUsername: 'Xeiiroa',
+    githubLink: 'https://github.com/Xeiiroa',
+    githubProfileImage: 'https://github.com/settings/profile',
+    linkedIn: os.getenv('LINKEDIN'),
+    }
 
+    return jsonify(profile)
 @api.route('/api/education')
 def education(): #returns information based on education
-    ...
+    education = os.getenv('EDUCATION_INFO')
 
 
 @api.route('/api/experience')
@@ -36,8 +42,24 @@ def experience(): #returns related work experience/ all work experience
     ...
 
 
-@api.route('/api/projects')
+@api.route('/api/projects', methods=['GET', 'POST'])
 def projects(): #returns information for projects 
+    Personal_Projects = [
+        {
+            title: 'Peeps',
+            year: 'August 2023',
+            description: 'A security camera software utilizing facial recognition for its captures',
+            technologies: ['Python', 'SQLite', 'Computer Vision', 'Twilio']
+            github_repo: 'https://github.com/Xeiiroa/Security-Camera'
+            
+    }
+        {
+            title: 'API Resume'
+            year: 'November 2023'
+            description: 'A resume converted to API Format',
+            technologies: ['Python', 'Flask', 'HTML', 'CSS'],
+            github_repo: 
+        }]
     """
     reason you did them  
     
